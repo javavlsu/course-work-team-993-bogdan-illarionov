392542a74ea72e37a075b7474a4ae89e37660324
w1sp <semen2002bs@gmail.com>
2023-06-07 12:34:11 +0300

    remove harmful annotations

--- casino-spring-mvc/src/main/java/com/company/storage/models/StorageLot.java
+++ casino-spring-mvc/src/main/java/com/company/storage/models/StorageLot.java
@@ -2,18 +2,12 @@ package com.company.storage.models;
 
 import com.company.models.casino.Lot;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.Set;
 import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "lots")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class StorageLot {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,7 +18,6 @@ public class StorageLot {
 
     @Column(name = "name")
     private String name;
-
     @Column(name = "description")
     private String description;
 
@@ -33,6 +26,23 @@ public class StorageLot {
 
     @OneToMany(mappedBy = "lot", fetch = FetchType.EAGER)
     private Set<StorageOutcome> outcomes;
+
+    public StorageLot(){
+
+    }
+
+    public StorageLot(
+            Long id,
+            String name,
+            String description,
+            Set<StorageGameOutcome> gameOutcomes,
+            Set<StorageOutcome> outcomes) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.gameOutcomes = gameOutcomes;
+        this.outcomes = outcomes;
+    }
 
     public Lot fromStorageModel(){
         return new Lot (
@@ -45,5 +55,49 @@ public class StorageLot {
                 this.getGameOutcomes().stream()
                         .map(StorageGameOutcome::fromStorageModel)
                         .collect(Collectors.toSet()));
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLink() {
+        return link;
+    }
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<StorageGameOutcome> getGameOutcomes() {
+        return gameOutcomes;
+    }
+    public void setGameOutcomes(Set<StorageGameOutcome> storageGameOutcomes) {
+        this.gameOutcomes = storageGameOutcomes;
+    }
+
+    public Set<StorageOutcome> getOutcomes() {
+        return outcomes;
+    }
+
+    public void setOutcomes(Set<StorageOutcome> outcomes) {
+        this.outcomes = outcomes;
     }
 }
